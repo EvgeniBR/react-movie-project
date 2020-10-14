b5f232be1124cccc58f5bd42db21dbf8388117c2
EvgeniBR <genib1989@gmail.com>
2020-10-14 09:09:38 +0300

    margin

--- src/api/Youtube_API.js
+++ src/api/Youtube_API.js
@@ -1,13 +1,15 @@
 import axios from 'axios'
 
-// const KEY = 'AIzaSyA_jEsAeTpTuOCg1W1C4P_AJ2PQmh-mgx4';
-const KEY2 = 'AIzaSyA_jEsAeTpTuOCg1W1C4P_AJ2PQmh-mgx4';
+const KEY = 'AIzaSyA_jEsAeTpTuOCg1W1C4P_AJ2PQmh-mgx4';
+const KEY2 = 'AIzaSyBWR9ojGgL6CvRXpQ66ZLW6Dlx4_qPo4ok';
+const KEY3 = 'AIzaSyDncr52pElQdDsbWCWUEofFEeL9pTk9tfM';
+
 export default axios.create({
     baseURL:'https://www.googleapis.com/youtube/v3' ,
     params : {
         part: 'snippet',
         type: 'video',
         maxResults: 1,
-        key:KEY2
+        key:KEY
     }
 })
